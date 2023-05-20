30c90316acd7c29987bc34d30a73743a56bffd47
akshayraoch <chakshayrao@gmail.com>
2023-05-20 11:29:50 +0530

    added spaces to the content component

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -19,9 +19,9 @@ const Content =(props)=>{
   
   return(
    <>
-     <Part name ={props.part1} exercise ={props.exercises1}/>
-     <Part name ={props.part2} exercise ={props.exercises2}/>
-     <Part name ={props.part3} exercise ={props.exercises3}/>
+     <Part name = {props.part1}  exercise ={props.exercises1}/>
+     <Part name = {props.part2}  exercise ={props.exercises2}/>
+     <Part name = {props.part3}  exercise ={props.exercises3}/>
    </>
   
   )
